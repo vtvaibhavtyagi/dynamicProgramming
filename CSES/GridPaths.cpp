df560a794f84f1022655a0ae7f2fc1455a11d5b5
Your Name <you@example.com>
2022-06-02 04:09:37 +0530

    june 2

---
+++ CSES/GridPaths.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked // faster char input
+#define fon(i, n) for (i = 0; i < n; i++)
+#define fn(i, j, n) for (i = j; i < n; i++)
+#define fr(i, n) for (i = n - 1; i >= 0; i--)
+#define ll long long int
+#define pb push_back
+#define mp make_pair // making pair
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define sv(v) sort(v.begin(), v.end());
+#define svd(v) sort(v.begin(), v.end(), greater<int>()); // sort vector decending
+#define svp(v, fun) sort(v.begin(), v.end(), fun);       // sort vector in a particular order
+#define sa(arr) sort(arr, arr + sizeof(arr) / sizeof(arr[0]));
+#define sad(arr) sort(arr, arr + sizeof(arr) / sizeof(arr[0]), greater<int>()); // sort arr decending
+const int mod = 1000000007;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+void showArray(ll a[], ll array_size)
+{
+    ll i;
+    fon(i, array_size) cout << a[i] << ' ';
+    cout << endl;
+}
+void showVector(vl v)
+{
+    for (ll x : v)
+        cout << x << ' ';
+    cout << endl;
+}
+int mpow(int base, int exp)
+{
+    base %= mod;
+    int result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+bool compareInterval(pl i1, pl i2) // function sort in a particular order
+{
+    return (i1.first < i2.first);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        ll n, i, j;
+        cin >> n;
+        bool mat[n + 1][n + 1];
+        int dp[n + 1][n + 1];
+
+        fn(i, 1, n + 1) fn(j, 1, n + 1)
+        {
+            char curr;
+            cin >> curr;
+            if (curr == '.')
+                mat[i][j] = 0;
+            else
+                mat[i][j] = 1;
+        }
+
+        fn(i, 1, n + 1) fn(j, 1, n + 1)
+        {
+            if (mat[i][j])
+            {
+                dp[i][j] = 0;
+                continue;
+            }
+
+            if (i == 1 && j == 1)
+            {
+                dp[i][j] = 1;
+                continue;
+            }
+            int a = (i == 1 ? 0 : dp[i - 1][j]);
+            int b = (j == 1 ? 0 : dp[i][j - 1]);
+            
+            dp[i][j] = (a + b) % mod;
+        }
+
+        cout << dp[n][n] << '\n';
+    }
+    return 0;
+}
---
+++ CSES/Removing Digits.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked // faster char input
+#define fon(i, n) for (i = 0; i < n; i++)
+#define fn(i, j, n) for (i = j; i < n; i++)
+#define fr(i, n) for (i = n - 1; i >= 0; i--)
+#define ll long long int
+#define pb push_back
+#define mp make_pair // making pair
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define sv(v) sort(v.begin(), v.end());
+#define svd(v) sort(v.begin(), v.end(), greater<int>()); // sort vector decending
+#define svp(v, fun) sort(v.begin(), v.end(), fun);       // sort vector in a particular order
+#define sa(arr) sort(arr, arr + sizeof(arr) / sizeof(arr[0]));
+#define sad(arr) sort(arr, arr + sizeof(arr) / sizeof(arr[0]), greater<int>()); // sort arr decending
+const int mod = 1000000007;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+void showArray(ll a[], ll array_size)
+{
+    ll i;
+    fon(i, array_size) cout << a[i] << ' ';
+    cout << endl;
+}
+void showVector(vl v)
+{
+    for (ll x : v)
+        cout << x << ' ';
+    cout << endl;
+}
+int mpow(int base, int exp)
+{
+    base %= mod;
+    int result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+bool compareInterval(pl i1, pl i2) // function sort in a particular order
+{
+    return (i1.first < i2.first);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t = 1;
+
+    while (t--)
+    {
+        int n, i;
+        cin >> n;
+        vi dp(n + 1);
+        dp[0] = 0;
+        fn(i, 1, n + 1)
+        {
+
+            int temp = i;
+            int minStep = INT_MAX;
+            while (temp != 0)
+            {
+                int curr = temp % 10;
+                temp /= 10;
+                if (curr == 0)
+                    continue;
+
+                minStep = min(minStep, 1 + dp[i - curr]);
+            }
+
+            dp[i] = minStep;
+        }
+
+        cout << dp[n] << '\n';
+    }
+    return 0;
+}
